bba18b07582c3ba515f94d5862233c4febaf93ff
sean watters <sean@watters.io>
2020-06-05 14:39:12 -0700

    fixed react imports

--- horizontal-bar-graph/src/index.tsx
+++ horizontal-bar-graph/src/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import * as React from 'react'
+
 import ChartBuilder from '@chartiful/react-chart-builder'
 import { VerticalBarGraphConfig, BaseChartConfig } from '../../types'
 
--- line-graph/src/index.tsx
+++ line-graph/src/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import * as React from 'react'
+
 import { LineGraphConfig } from '../../types'
 import ChartBuilder from '@chartiful/react-chart-builder'
 
